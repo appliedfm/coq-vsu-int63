de8e6d86f930b6ba39e20c9b384e55d4da434d52
Timothy Carstens <intoverflow@gmail.com>
2022-01-15 18:08:26 -0800

    s/export/global

--- theories/Int63/c/ast/ast.v
+++ theories/Int63/c/ast/ast.v
@@ -2,5 +2,5 @@ From VST Require Export floyd.proofauto.
 From VST Require Export floyd.library.
 From Int63 Require Export c.clightgen.int63.
 
-#[export]Instance CompSpecs : compspecs. make_compspecs prog. Defined.
+#[global]Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
